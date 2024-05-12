686945c9f589e44e59050826b012e9e512e0de77
kooza-colline <koozacollinz1@gmail.com>
2024-05-12 17:18:17 +0300

    fixed bugs

--- components/cards/CardsShare.tsx
+++ components/cards/CardsShare.tsx
@@ -72,7 +72,7 @@ export default function CardsShare({ checkOut = [], students = [] }: any) {
   return (
    <div>
     <div className='flex justify-between items-center'>
-      <h2 className='flex items-center gap-2 text-sm'> <Calendar className='text-[#9333ea]' size={16}/>today's attendance</h2>
+      <h2 className='flex items-center gap-2 text-sm'> <Calendar className='text-[#9333ea]' size={16}/>todays attendance</h2>
       <div className='flex items-center gap-2'>
      <Link href="/dashboard/attendence">
      <Button className='bg-white text-xs dark:bg-black text-black dark:text-white shadow1 flex items-center gap-2 dark:hover:bg-amber-500 hover:bg-amber-500 hover:text-white'><Plus size={12}/> New attendance</Button>
@@ -102,7 +102,9 @@ export default function CardsShare({ checkOut = [], students = [] }: any) {
       {latestArrivals.length === 0 ? (
     <Card className='lg:w-[60%] w-full lg:h-[25%] h-[20%] rounded-xl supports-[backdrop-filter]:bg-background/60 bg-background/95 backdrop-blur'>
      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-     <CardTitle className="text-sm font-medium">{new Date().toLocaleDateString().replace(/'/g, "&apos;")}</CardTitle>
+     <CardTitle className="text-sm font-medium">
+     {new Date().toLocaleDateString().replace(/'/g, "&apos;")}
+           </CardTitle>
 
 
 
@@ -111,7 +113,7 @@ export default function CardsShare({ checkOut = [], students = [] }: any) {
     <CardContent>
       <div className="lg:text-2xl text-lg font-bold">No Arrivals today</div>
       <p className="text-xs text-muted-foreground">
-        We haven't recorded any arrivals today
+        We havent recorded any arrivals today
       </p>
     </CardContent>
   </Card>
@@ -122,7 +124,9 @@ export default function CardsShare({ checkOut = [], students = [] }: any) {
       .map((student: Student) => (
       <Card key={student.id} className='lg:w-[60%] w-full lg:h-[25%] h-[20%] rounded-xl supports-[backdrop-filter]:bg-background/60 bg-background/95 backdrop-blur'>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">{new Date().toLocaleDateString().replace(/'/g, "&apos;")}</CardTitle>
+        <CardTitle className="text-sm font-medium">
+        {new Date().toLocaleDateString().replace(/'/g, "&apos;")}
+                  </CardTitle>
 
           <h2 className='font-semibold'>{new Date(student.checkIn).toLocaleTimeString()}</h2>
         </CardHeader>
@@ -146,7 +150,7 @@ export default function CardsShare({ checkOut = [], students = [] }: any) {
         These are the latest check ins recorded today
         </p>
         <Link href="/dashboard/attendence">
-        <Button className='hover:text-white text-xs hover:bg-black text-black font-semibold shadow1 flex items-center gap-2 dark:hover:text-white bg-amber-500 dark:text-black  mt-4'><Plus size={12}/> New attendance</Button>
+        <Button className='hover:text-white text-xs hover:bg-black text-black font-semibold shadow1 flex items-center gap-2 dark:hover:text-white bg-amber-500 dark:text-black  mt-4'><Plus size={12}/>New attendance</Button>
         </Link>
       </CardContent>
         </Card>
@@ -155,77 +159,3 @@ export default function CardsShare({ checkOut = [], students = [] }: any) {
    </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // <Card>
-    //   <CardHeader className="pb-3">
-    //     <CardTitle>Latest Arrivals Today</CardTitle>
-    //     <CardDescription>
-    //       Latest Desishub Arrivals Today and their time.
-    //     </CardDescription>
-    //   </CardHeader>
-    //   <CardContent>
-    //     <Separator className="my-4" />
-    //     <div className="space-y-4">
-    //       <h4 className="text-sm font-medium">Desishub Students</h4>
-    //       {isLoading ? (
-    //         <p>Loading...</p>
-    //       ) : latestArrivals.length === 0 ? (
-    //         <p>No arrivals yet today.</p>
-    //       ) : (
-    //         <div className="grid gap-6">
-    //           {latestArrivals.map((student) => (
-    //             <div
-    //               key={`${student.id}-${student.checkIn}`}
-    //               className="flex items-center justify-between space-x-4"
-    //             >
-    //               <div className="flex items-center space-x-4">
-    //                 <Avatar>
-    //                   <AvatarImage
-    //                   className='object-cover'
-    //                     src={student.image}
-    //                     alt={student.name}
-    //                   />
-    //                   <AvatarFallback>
-    //                     {student.name.slice(0, 2).toUpperCase()}
-    //                   </AvatarFallback>
-    //                 </Avatar>
-    //                 <div>
-    //                   <p className="text-sm font-medium leading-none">
-    //                     {student.name}
-    //                   </p>
-    //                   <p className="text-sm text-muted-foreground">
-    //                     {student.email}
-    //                   </p>
-    //                 </div>
-    //               </div>
-    //               <div>
-    //                 <p className="text-sm text-muted-foreground">
-    //                   {new Date(student.checkIn).toLocaleTimeString()}
-    //                 </p>
-    //               </div>
-    //             </div>
-    //           ))}
-    //         </div>
-    //       )}
-    //     </div>
-    //   </CardContent>
-    // </Card>
